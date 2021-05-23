f42d005fdef7a1c245fcc961cff78265f8dfff4e
Rodolfo Alcaparra <rodolfoalcaparra@mail.com>
2021-05-23 19:38:06 +0200

    He hecho la depuración de código, ahora todo funciona correctamente

--- src/operaciones/Main.java
+++ src/operaciones/Main.java
@@ -22,11 +22,11 @@ public class Main {
         System.out.println(resultado);
         
         //prueba mayor:
-        int resultado2 = operaciones.mayor(8,8);    
+        int resultado2 = operaciones.mayor(8,5);    
         System.out.println(resultado2);
         
         //prueba sumarVector:
-        int[] vector = {4,2,4,6};
+        int[] vector = {1, 2, 3, 4};
         int resultado3 = operaciones.sumarVector(vector);    
         System.out.println(resultado3);
         
